28f74a932781b68843c12fcadb79d37b6e89e701
kylieogg <kylieogg1@gmail.com>
2022-04-19 16:42:17 -0400

    updated server.js with mysql expression

--- server.js
+++ server.js
@@ -2,10 +2,24 @@ const express = require('express');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
+const mysql = require('mysql2');
 
 // Express middleware
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+
+// Connect to database
+const db = msql.createConnection(
+    {
+        host: 'localhost',
+        // Your MySQL username,
+        user: 'root',
+        // Your MySQL password
+        password: '',
+        database: 'election'
+    },
+    console.log('Connecte to the election database.')
+);
 
 // Default response for any other request (Not Found)
 app.use((req, res) => {
